35aff4bc1942573e707d6138c62d43c5628651ac
wuchvi <wuchvi@qq.com>
2021-04-16 23:48:05 +0800

    feat:增加等待接听视频通话的弹窗

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -3,9 +3,11 @@ import { notification } from 'antd';
 import { AudioOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { useSubject, ofAction } from '@src/hooks/useSubject';
 import { openVideoCallWindow } from '@src/utils';
+import { MessageCenter, ESignalType } from '@src/modules/RemoteGlobal';
 
 import { AudioModal } from './AudioModal';
 import { Uploader } from './Uploader';
+import { VideoInvitationModal } from './VideoInvitationModal';
 
 import './style.less';
 
@@ -25,6 +27,10 @@ export const Toolbar: React.FC<IToolbar> = React.memo(
   ({ sendMessage, uid, currentTo }) => {
     const [globalSubject$, RxEvent] = useSubject();
     const [isAudioShow, setIsAudioShow] = React.useState<boolean>(false);
+    const [
+      isVideoCallInviteShow,
+      setIsVideoCallInviteShow,
+    ] = React.useState<boolean>(false);
 
     React.useEffect(() => {
       const audioCloseSub = globalSubject$
@@ -35,34 +41,55 @@ export const Toolbar: React.FC<IToolbar> = React.memo(
     }, []);
 
     const showAudioModal = React.useCallback(() => {
-      if (!isAudioShow) {
-        setIsAudioShow(true);
-        notification.open({
-          key: 'audio',
-          message: (
-            <AudioModal
-              sendMessage={sendMessage}
-              uid={uid}
-              currentTo={currentTo}
-            />
-          ),
-          placement: 'bottomRight',
-          duration: null,
-          className: 'audio-modal',
-          bottom: 170,
-          onClose: () => {
-            setIsAudioShow(false);
-            globalSubject$.next({ action: RxEvent.AUDIO_CLOSE });
-          },
-        });
-      }
+      setIsAudioShow(true);
+      notification.open({
+        key: 'audio',
+        message: (
+          <AudioModal
+            sendMessage={sendMessage}
+            uid={uid}
+            currentTo={currentTo}
+          />
+        ),
+        placement: 'bottomRight',
+        duration: null,
+        className: 'audio-modal',
+        bottom: 170,
+        onClose: () => {
+          setIsAudioShow(false);
+          globalSubject$.next({ action: RxEvent.AUDIO_CLOSE });
+        },
+      });
     }, [isAudioShow]);
+
+    const initiateVideoCall = React.useCallback(() => {
+      setIsVideoCallInviteShow(true);
+      MessageCenter.sendSignal({
+        type: ESignalType.INITIATE_VIDEO_CALL,
+        payload: { fromId: uid, toId: currentTo },
+      });
+      notification.open({
+        key: 'initiateVideoCall',
+        message: <VideoInvitationModal />,
+        className: 'video-invitation-modal',
+        placement: 'bottomRight',
+        duration: null,
+        bottom: 170,
+        onClose: () => {
+          setIsVideoCallInviteShow(false);
+        },
+      });
+      // openVideoCallWindow(false);
+    }, [uid, currentTo]);
 
     return (
       <div className="toolbar">
-        <AudioOutlined onClick={showAudioModal} />
+        <AudioOutlined onClick={showAudioModal} disabled={isAudioShow} />
         <Uploader sendMessage={sendMessage} />
-        <VideoCameraOutlined onClick={() => openVideoCallWindow(false)} />
+        <VideoCameraOutlined
+          onClick={initiateVideoCall}
+          disabled={isVideoCallInviteShow}
+        />
       </div>
     );
   }
